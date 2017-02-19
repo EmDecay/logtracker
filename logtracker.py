e08ce4a755d793226efcc469545bb0e98ddd4de3
Matt <emdecay@protonmail.com>
2017-02-19 14:34:13 -0800

    Added comments for the sake of code clarity

--- logtracker.py
+++ logtracker.py
@@ -3,23 +3,28 @@
 
 import os, csv, time, sqlite3, dbinit, glob
 
+# logdir_path is the path for the individual log directories, assuming a directory is created to hold the logs of each individual server or group of servers
 logdir_path = "/var/log"
 logdirs = os.listdir(logdir_path)
+
+# For logs that are rotated hourly, determine whether a system is logging based on whether or not it logged during the previous hour
 prevhour = int(time.strftime('%H'))-1
 if prevhour == -1:
     prevhour = 23
-print prevhour
+
+# Connect to the database (previously initialized through dbinit.py)
 dbconn = sqlite3.connect('logtracker.db')
 dbc = dbconn.cursor()
 
-# Stuff for CSV support; remove or modify when getting rid of CSV support
+# Stuff for CSV output of results
 logfile = time.strftime("%Y-%m-%d_%H-%M-%S_") + "results.csv"
 file = open(logfile, 'ab')
 csvout = csv.writer(file)
 
 # Create record for the scan
 dbc.execute("INSERT INTO scan VALUES(\'" + str(time.strftime("%X")) + "\', \'" + str(time.strftime("%x")) + "\', NULL)")
-# Get the scanid
+
+# Get the scanid; used when adding records for each individual scan result
 curscan = dbc.execute("SELECT MAX(scanid) FROM scan").fetchone()[0]
 
 for dir in logdirs:
@@ -30,11 +35,14 @@ for dir in logdirs:
     dirpath = logdir_path + "/" + dir + "/"
     for fileobj in glob.glob(dirpath + day + "/" + str(prevhour).zfill(2) + "/*"):
         if os.path.isfile(fileobj):
+			# System is logging
             logging = "Yes"
     for fileobj in glob.glob(dirpath + day + "/*"):
         if os.path.isfile(fileobj):
+			# System is logging
             logging = "Yes"
     if logging == "No":
+		# System is not logging; see if it was logging the last time logtracker was run
         prevresult = dbc.execute("SELECT status FROM scanresult WHERE dir = \'" + dir + "\' AND scanid =\'" + str(curscan-1) + "\'").fetchone()
         if prevresult is not None:
             prevresult = prevresult[0]
